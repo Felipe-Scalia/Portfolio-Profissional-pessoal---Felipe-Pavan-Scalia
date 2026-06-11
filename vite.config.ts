8624f229bf304dcd376731c53fe430e89383fef7
Felipe-Scalia <felipeps1411@gmail.com>
2026-06-10 21:30:52 -0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/Portfolio-Profissional-pessoal---Felipe-Pavan-Scalia/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
